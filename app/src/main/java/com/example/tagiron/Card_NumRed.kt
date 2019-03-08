8095d192c7ce2d3ff5cca49db1fdc312a0324a32
YTanaka <pirori_nyanko_033014@gmail.com>
2019-03-09 03:01:28 +0900

    [add]質問カードを一枚追加 "赤の数字タイルは何枚ある？"

---
+++ app/src/main/java/com/example/tagiron/Card_NumRed.kt
@@ -0,0 +1,34 @@
+package com.example.tagiron
+
+/**
+ * 質問カード
+ * "赤の数字タイルは何枚ある？"
+ */
+class Card_NumRed() : Card {
+    /**
+     * 質問文
+     */
+    override val questionStr: String = "赤の数字タイルは何枚ある？"
+
+    /**
+     * カードの動作
+     * @param hand: List<NumberTile> プレイヤーの手元の数字タイル
+     * @return String 質問に対する回答の文章
+     */
+    override fun action(hand: List<NumberTile>): String {
+        var redNum = 0  // 赤色のカードの枚数
+
+        // 数字タイルの赤色のタイルの枚数をカウント
+        for (numberTile in hand){
+            when(numberTile.color){
+                "Red" -> {
+                    redNum++
+                }
+            }
+        }
+
+        // 結果を戻り値にする
+        return "赤色の数字タイルは${redNum}枚です"
+    }
+
+}
