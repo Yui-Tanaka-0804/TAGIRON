50b431892ec2762e0188765390b541967bd54258
YTanaka <pirori_nyanko_033014@gmail.com>
2019-03-09 16:41:15 +0900

    [add]プレイヤーのクラスを追加

---
+++ app/src/main/java/com/example/tagiron/Player.kt
@@ -0,0 +1,20 @@
+package com.example.tagiron
+
+/**
+ * プレイヤーの行動
+ *
+ * @param hand 手札の数字タイル
+ */
+class Player(private val hand: List<NumberTile>){
+
+    /**
+     * 選択されたカードへの回答
+     *
+     * @param card 選択されたカード
+     * @return 回答文
+     */
+    fun answer(card: Card): String{
+        return card.action(hand)
+    }
+
+}
