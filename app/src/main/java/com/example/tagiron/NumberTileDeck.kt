33c1e805c0554a2200769ebd1cb88647fe2a6cd6
YTanaka <pirori_nyanko_033014@gmail.com>
2019-03-09 15:40:56 +0900

    [fix]取得した数字タイルが山札から削除されない不具合を修正

--- app/src/main/java/com/example/tagiron/NumberTileDeck.kt
+++ app/src/main/java/com/example/tagiron/NumberTileDeck.kt
@@ -5,7 +5,7 @@ package com.example.tagiron
  */
 class NumberTileDeck{
     // いずれ山札の枚数変えたりしたいからめんどくさいことしてる
-    val tileDeck = mutableListOf<NumberTile>()    // 数字タイルの山札
+    var tileDeck = listOf<NumberTile>()    // 数字タイルの山札
     val tileColor = arrayOf("Red", "Blue", "Yellow")    // 使用する色(黄色は最後に配置する)
     val tileNum = Pair(0, 9)    // 数字の最小、最大値
     val tileMiddleNum = tileNum.second - tileNum.first / 2 + 1  // 中央の数字(5)
@@ -14,17 +14,22 @@ class NumberTileDeck{
      * 初期化時に山札を作成
      */
     init{
+        val initTileDeck = mutableListOf<NumberTile>()
+
         // 数字タイルの山札を作成
         // 色ごとに作る
         for (color in 0..tileColor.count()-2) {
             // 数字は指定した範囲で作る
             for (number in tileNum.first..tileNum.second) {
                 // 数字が5の時だけ黄色にする
-                tileDeck.add(NumberTile(number, tileColor[if (number == tileMiddleNum) tileColor.count()-1 else color]))
+                initTileDeck.add(NumberTile(number, tileColor[if (number == tileMiddleNum) tileColor.count()-1 else color]))
             }
         }
         // 数字タイルをシャッフルしておく
-        tileDeck.shuffle()
+        initTileDeck.shuffle()
+
+        // プロパティに代入
+        tileDeck = initTileDeck
     }
 
     /**
@@ -37,7 +42,7 @@ class NumberTileDeck{
         // 数字タイルを指定された枚数取得する
         val getTiles = tileDeck.take(num)
         // 取得されたタイルは破棄(もっといい方法ある気がする)
-        tileDeck.drop(num)
+        tileDeck = tileDeck.drop(num)
 
         // 取得したタイルを戻り値にする
         return getTiles
